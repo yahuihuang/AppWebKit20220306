48e7db99bbe2f59b0a8f9769187096a048364823
Huang Grace <huang.yahui.grace@gmail.com>
2022-03-06 11:40:11 +0800

    GO url

--- AppWebKit20220306/ViewController.swift
+++ AppWebKit20220306/ViewController.swift
@@ -10,17 +10,28 @@ import WebKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var goButton: UIButton!
+    @IBOutlet weak var urlInput: UITextField!
     @IBOutlet weak var myWebView: WKWebView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        if let url = URL(string: "https://tw.yahoo.com") {
+        if let url = URL(string: "https://warrant.tssco.com.tw") {
             let request = URLRequest(url: url)
             myWebView.load(request)
         }
     }
 
-
+    @IBAction func goAction(_ sender: Any) {
+        var urlString = urlInput.text ?? ""
+        urlString = "https://" + urlString
+        
+        if let url = URL(string: urlString) {
+            let request = URLRequest(url: url)
+            myWebView.load(request)
+        }
+    }
+    
 }
 
